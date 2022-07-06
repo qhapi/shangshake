42cc52876d5221f56a0c362a4f35c1b6f8f5fb8d
OrlokTheEternal <2380687229@qq.com>
2022-07-06 21:36:50 +0800

    为sctDAO类加上了passed属性

--- src/main/java/com/tedu/shangshake/service/impl/StudentCreditServiceImpl.java
+++ src/main/java/com/tedu/shangshake/service/impl/StudentCreditServiceImpl.java
@@ -1,17 +1,16 @@
 package com.tedu.shangshake.service.impl;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
-import com.tedu.shangshake.mapper.CourseMapper;
-import com.tedu.shangshake.mapper.SpecialtyKindCreditMapper;
-import com.tedu.shangshake.mapper.SpecialtyMapper;
-import com.tedu.shangshake.mapper.StudentMapper;
+import com.tedu.shangshake.mapper.*;
 import com.tedu.shangshake.pojo.*;
 import com.tedu.shangshake.service.StudentCreditService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Service
 public class StudentCreditServiceImpl implements StudentCreditService {
@@ -23,6 +22,10 @@ public class StudentCreditServiceImpl implements StudentCreditService {
     StudentMapper studentMapper;
     @Autowired
     CourseMapper courseMapper;
+    @Autowired
+    KindMapper kindMapper;
+    @Autowired
+    StudentCourseTeacherMapper studentCourseTeacherMapper;
 
     //输入学生no，返回学生的总的学分修读情况。
     //QueryWrapper并不能真正的多表查询。
@@ -40,13 +43,30 @@ public class StudentCreditServiceImpl implements StudentCreditService {
         queryWrapper.eq("spno", spNo);
         List<SpecialtyKindCreditDAO> spkcDAOList = specialtyKindCreditMapper.selectList(queryWrapper);
 
+        //遍历类型
         for (SpecialtyKindCreditDAO spkcDAO : spkcDAOList) {
             //查询出专业名
             queryWrapper = new QueryWrapper();
             queryWrapper.eq("no", spkcDAO.getKno());
             SpecialtyDAO specialtyDAO = specialtyMapper.selectOne(queryWrapper);
             AllConditionVO acVO = new AllConditionVO();
-            acVO.setkName(specialtyDAO.getSpname());
+            acVO.setkName(specialtyDAO.getSpname());//设置专业名
+            acVO.setAllCredit(spkcDAO.getCredit());//设置总学分
+        }
+
+        //获取学生学到的所有课程，累加指定类型的课程的学分
+        List<KindDAO> kindDAOList = kindMapper.selectList(null);
+        Map<Integer, Float> kindCreditMap = new HashMap<>();//k是kno,v是credit
+        for (KindDAO kindDAO : kindDAOList) {
+            kindCreditMap.put(kindDAO.getKno(), 0f);
+        }
+
+        Map<Integer, Integer> cPassMap = new HashMap<>();
+        queryWrapper = new QueryWrapper();
+        queryWrapper.eq("sno", no);
+        List<StudentCourseTeacherDAO> sctDAOList = studentCourseTeacherMapper.selectList(null);
+        for (StudentCourseTeacherDAO sctDAO : sctDAOList) {
+            cPassMap.put(sctDAO.getCno(), sctDAO.getPassed());
         }
 
         return null;
